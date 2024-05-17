4139b0fc739db84e09626caab8334926f881775e
katann-IWATATE-tann <104120192+katann-tann@users.noreply.github.com>
2024-05-17 16:07:09 +0900

    計算

--- cs0517/Form1.cs
+++ cs0517/Form1.cs
@@ -6,5 +6,36 @@ namespace cs0517
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Top = label1.Top + 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("‚±‚ñ‚É‚¿‚Í");   
+            MessageBox.Show(100.ToString());
+            MessageBox.Show($"{100}");
+            MessageBox.Show($"label1. Top={label1.Top}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
     }
 }
